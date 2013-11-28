55ce54fe77c9d1589b854e491f01760914eed4de
Simon Vandel Sillesen <simon.vandel@gmail.com>
2013-11-28 09:40:39 +0100

    indledende kode til generering af en random graf

---
+++ Source/graf_gen.c
@@ -0,0 +1,296 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define NUM_VERTEX 100
+#define MAX_WEIGHT 5
+#define MAX_EDGES_PER_VERTEX 3
+#define MAX_X 10
+#define MAX_Y 10
+#define MAX_FLOORS 2
+
+typedef struct Vertex Vertex;
+typedef struct Edge Edge;
+typedef struct Graph Graph;
+
+struct Edge {
+    int weight;
+    int color;
+    Vertex *connectingVertex1;
+    Vertex *connectingVertex2;
+};
+
+struct Vertex {
+    int id;
+    int x;
+    int y;
+    int building;
+    int type;
+    int floor;
+    int degree; /* number of edges */
+    Edge **edges;
+};
+
+struct Graph {
+    Vertex **vertices;
+};
+
+
+
+Graph *makeGraph(void);
+Vertex *createRandomVertex(Graph *g, int id);
+void addVertexToGraph(Graph *g, Vertex *v, int i);
+Edge *createWeightedEdge(Graph *g);
+void addEdgeToVertex(Vertex *v, Edge *e, int i);
+Vertex *getVertex(const Graph *g, int index);
+void printGraph(Graph *g);
+void setConnectingVertexInEdges(Graph *g);
+void printXML(Graph *g);
+void xmlOpenTag(char *name, FILE *xml);
+void xmlCloseTag(char *name, FILE *xml);
+void xmlWriteInt(char *format, int var, FILE *xml);
+
+int main(void) {
+    srand(time(NULL));
+    Graph *g = makeGraph();
+
+    printXML(g);
+
+    //printGraph(g);
+}
+
+void printXML(Graph *g) {
+    FILE *xml = fopen("graph.xml", "w");
+
+    xmlOpenTag("graph", xml);
+
+    int i;
+    for (i = 0; i < NUM_VERTEX; i++) {
+        int j;
+        Vertex *v = getVertex(g, i);
+        xmlOpenTag("vertex", xml);
+
+        xmlOpenTag("id", xml);
+        xmlWriteInt("%d", v->id, xml);
+        xmlCloseTag("id", xml);
+
+        xmlOpenTag("x", xml);
+        xmlWriteInt("%d", v->x, xml);
+        xmlCloseTag("x", xml);
+
+        xmlOpenTag("y", xml);
+        xmlWriteInt("%d", v->y, xml);
+        xmlCloseTag("y", xml);
+
+        xmlOpenTag("floor", xml);
+        xmlWriteInt("%d", v->floor, xml);
+        xmlCloseTag("floor", xml);
+
+        xmlOpenTag("degree", xml);
+        xmlWriteInt("%d", v->degree, xml);
+        xmlCloseTag("degree", xml);
+
+        xmlOpenTag("edges", xml);
+
+        for (j = 0; j < v->degree; j++) {
+            Edge *e = v->edges[j];
+            xmlOpenTag("edge", xml);
+
+            xmlOpenTag("weight", xml);
+            xmlWriteInt("%d", e->weight, xml);
+            xmlCloseTag("weight", xml);
+
+            xmlOpenTag("connectingVertex1ID", xml);
+            xmlWriteInt("%d", e->connectingVertex1->id, xml);
+            xmlCloseTag("connectingVertex1ID", xml);
+
+            xmlOpenTag("connectingVertex2ID", xml);
+            xmlWriteInt("%d", e->connectingVertex2->id, xml);
+            xmlCloseTag("connectingVertex2ID", xml);
+
+            xmlCloseTag("edge", xml);
+        }
+
+        xmlCloseTag("edges", xml);
+
+
+        xmlCloseTag("vertex", xml);
+    }
+
+
+    xmlCloseTag("graph", xml);
+
+    fclose(xml);
+}
+
+void xmlWriteInt(char *format, int var, FILE *xml) {
+    char buffer[100];
+    sprintf(buffer, format, var);
+    fputs(buffer, xml);
+}
+
+void xmlCloseTag(char *name, FILE *xml) {
+    char buffer[100];
+    sprintf(buffer, "</%s>", name);
+    fputs(buffer, xml);
+}
+
+void xmlOpenTag(char *name, FILE *xml) {
+    char buffer[100];
+    sprintf(buffer, "<%s>", name);
+    fputs(buffer, xml);
+}
+
+Graph *makeGraph(void) {
+    int i;
+    Graph *g = (Graph *) malloc(sizeof(Graph));
+    g->vertices = (Vertex **) malloc(sizeof(Vertex *) * NUM_VERTEX);
+
+    for (i = 0; i < NUM_VERTEX; i++) {
+        Vertex *v = createRandomVertex(g, i);
+        addVertexToGraph(g, v, i);
+    }
+
+    setConnectingVertexInEdges(g);
+
+    return g;
+}
+
+void setConnectingVertexInEdges(Graph *g) {
+    int j;
+    int i;
+
+    for (i = 0; i < NUM_VERTEX; i++) {
+
+        Vertex *v = getVertex(g, i);
+
+        for (j = 0; j < v->degree; j++) {
+            /* add connectingVertex2 vertex to edges */
+
+            int random;
+            Edge *e;
+
+            /* continue to getVertex until a vertex has been generated
+               that is unique to v */
+            do {
+                random = rand() % NUM_VERTEX;
+                e = v->edges[j];
+                e->connectingVertex1 = v;
+                e->connectingVertex2 = getVertex(g, random);
+            } while (e->connectingVertex2->id == v->id);
+        }
+    }
+}
+
+Vertex *createRandomVertex(Graph *g, int id) {
+    int i;
+    Vertex *v = (Vertex *) malloc(sizeof(Vertex));
+
+    /* randomly select number of edges */
+    int numEdges = rand() % MAX_EDGES_PER_VERTEX + 1;
+
+    /* set degree */
+    v->degree = numEdges;
+
+    v->edges = (Edge **) malloc(sizeof(Edge *) * numEdges);
+
+    /* create edges */
+    for (i = 0; i < numEdges; i++) {
+        Edge *e = createWeightedEdge(g);
+        addEdgeToVertex(v, e, i);
+    }
+
+    /* set x */
+    int x = rand() % MAX_X + 1;
+    v->x = x;
+
+    /* set y */
+    int y = rand() % MAX_Y + 1;
+    v->y = y;
+
+    /* set floor */
+    int floor = id % MAX_FLOORS + 1;
+    v->floor = floor;
+
+
+    /* set id */
+    v->id = id;
+
+    return v;
+}
+
+void addVertexToGraph(Graph *g, Vertex *v, int i) {
+    g->vertices[i] = v;
+}
+
+Edge *createWeightedEdge(Graph *g) {
+    Edge *e = (Edge *) malloc(sizeof(Edge));
+
+    int weight = rand() % MAX_WEIGHT + 1;
+    e->weight = weight;
+
+    return e;
+}
+
+void addEdgeToVertex(Vertex *v, Edge *e, int i) {
+    v->edges[i] = e;
+}
+
+Vertex *getVertex(const Graph *g, int index) {
+    return g->vertices[index];
+}
+
+int getWeight(Vertex *v1, Vertex *v2) {
+    int i;
+    int degree = v1->degree;
+    for (i = 0; i < degree; i++) {
+        Edge *e = v1->edges[i];
+        if (e->connectingVertex2->id == v2->id) {
+            return e->weight;
+        }
+    }
+
+    return 0;
+}
+
+void printGraph(Graph *g) {
+
+    /* print header */
+    printf("%-6s", "");
+    int i;
+    int j;
+    for (i = 0; i < NUM_VERTEX; i++) {
+        int id = getVertex(g, i)->id;
+        printf("%-3d", id + 1);
+    }
+    printf("\n");
+
+
+    printf("\n");
+
+    /* print lines containing each node info
+       i is the vertices displayed horizontally
+       j is the vertices displayed vertically
+       k is the edge index of vertex j */
+
+    for (j = 0; j < NUM_VERTEX; j++) {
+        Vertex *v = getVertex(g, j);
+        /* print id first */
+        int id = v->id;
+        printf("%-6d", id + 1);
+
+        /* print weights */
+        for (i = 0; i < NUM_VERTEX; i++) {
+            Vertex *vToCheck = getVertex(g, i);
+            int weight = getWeight(v, vToCheck);
+
+            if (weight > 0) {
+                printf("%-3d", weight);
+            } else {
+                printf("%-3d", 0);
+            }
+        }
+
+        printf("\n");
+    }
+}
