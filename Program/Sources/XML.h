e7625bec970c2cd3eacf3565e5afe472838152e8
zanderdk <moonfrogdk@gmail.com>
2013-12-09 13:51:09 +0100

    fix

--- Program/Sources/XML.h
+++ Program/Sources/XML.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "graph.h"
 
 Floor *readXml(FILE *fp);
@@ -11,17 +8,13 @@ int reachChar(char t, FILE *fp);
 void nextElement(char *name, FILE *fp);
 int readToChar(char *name, char t, FILE *fp);
 void readEdges(FILE *fp, Edge *edges, int numberOfEdges);
-void readvertices(FILE *fp, Vertex *vertices, int numberOfvertices, Edge *ep,
-                  int numberOfEdges);
+void readvertices(FILE *fp, Vertex *vertices, int numberOfvertices, Edge *ep, int numberOfEdges);
 int nextElementValue(char *name, FILE *fp);
 void assignEdge(EdgePointer **ep, EdgePointer *setEp, Edge *edge);
-void crawlEdges(Edge **edges, Vertex *vertices, int numberOfEdges,
-                int numberOfvertices);
+void crawlEdges(Edge **edges, Vertex *vertices, int numberOfEdges, int numberOfvertices);
 void crawVertices(Vertex **vertices, int numberOfvertices);
 void getFloorAndtype(int id, int *floor, int *type);
-void crawlFloors(Floor **floors, Vertex *vertices, int numberOfFloors,
-                 int numberOfvertices);
+void crawlFloors(Floor **floors, Vertex *vertices, int numberOfFloors, int numberOfvertices);
 Edge readEdge(FILE *fp);
-Vertex readVertex(FILE *fp, Edge *ep, int numberOfEdges,
-                  EdgePointer *EdgePointerStart);
+Vertex readVertex(FILE *fp, Edge *ep, int numberOfEdges, EdgePointer *EdgePointerStart);
 Edge *getEp(Edge *ep, unsigned int id, int numberOfEdges);
