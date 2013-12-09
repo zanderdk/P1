628ea3e3c0223398bb7c30fdc4535def2640aae5
cnHeider <mrnaah@gmail.com>
2013-12-09 19:47:31 +0100

    REMOVEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EVERYTHING!

--- Source/lala.h
+++
@@ -1,9 +0,0 @@
-#include "testGraph.h"
-
-int SmallestDist(unsigned int *dist, unsigned int *visited, unsigned int V);
-int Contains(unsigned int *list, unsigned int id,  unsigned int V);
-void InsertIntoList(unsigned int *list, unsigned int id, unsigned int V);
-void RemoveFromList(unsigned int *list, unsigned int id, unsigned int V);
-int IsEmpty(int *list, unsigned int V);
-void PrintList(unsigned int *list, unsigned int V);
-int Dijkstra(Graph *graph, int sourceId, int targetId, int *pathList);
